48ad9fb2792827899045445292efb334f550133c
Miguel Simão <masimao@sapo.pt>
2017-03-06 16:38:19 +0000

    Add files via upload

---
+++ test.m
@@ -0,0 +1,24 @@
+%clear all; close all; clc;
+
+instrreset;
+liberty=Liberty('/dev/ttyUSB0');
+liberty.connect;
+liberty.stream;
+k=tic;
+previousTime=toc(k);
+currentTime=toc(k);
+timeElapsed=currentTime-previousTime;
+
+while(currentTime<Inf)
+    
+    currentTime=toc(k);
+    timeElapsed=currentTime-previousTime;
+    
+    if(timeElapsed>0.01)
+        previousTime=currentTime;
+        x=liberty.data2(3:end,1)';
+        fprintf('%3.2f ',x); fprintf('\n');
+    end
+end
+
+fclose(liberty);
